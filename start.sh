15a3f0c11e7a5cccec98d31c23368fe16061ccd4
mokesd <95740613+mokesd@users.noreply.github.com>
2022-03-22 10:26:09 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=77b37bea-22c9-4662-8d82-9ea5d056cba6
 CADDYIndexPage=https://github.com/mokesd/remberme/3D.zip
-CONFIGCADDY=https://raw.githubusercontent.com/mokesd/remberme/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/mokesd/remberme/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/mokesd/huohuocare/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/mokesd/huohuocare/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/mokesd/remberme/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/mokesd/huohuocare/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
